4977a32d6ee77629daf8ad63caf270e37a7778c3
SaifAyman23 <139820748+SaifAyman23@users.noreply.github.com>
2024-01-31 19:59:02 +0200

    Displayed the categories in the home page

--- project/views.py
+++ project/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
-def home(request):
-    
-    return render(request,'pages/home.html',{})
+from category.models import Category
+def home(request): 
+    category =Category.objects.all
+    context={
+        'category':category,
+    }
+    return render(request,'pages/home.html',context)
